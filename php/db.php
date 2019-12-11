5dfea8ebf170fd9a604ca2f121df927ef0909798
YuHan <taiwansmile@gmail.com>
2019-12-11 11:40:43 +0800

    new heroku cleardb configuration

--- php/db.php
+++ php/db.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
-$hostname = "localhost";
-$dbuser = "admin";
-$dbpw = "123456";
-$dbname = "forum";
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+$hostname = $cleardb_server ;
+$dbuser = $cleardb_username;
+$dbpw = $cleardb_password;
+$dbname = $cleardb_db;
 
 $_SESSION["conn"] = mysqli_connect($hostname, $dbuser, $dbpw, $dbname);
 if($_SESSION["conn"]) {
